2b5ca5f26f9f33b9dac565fb04904cdfdcb4552e
Russell Wallace <russell.wallace@gmail.com>
2023-12-16 00:40:17 +0000

    fmt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 class Program {
+	static void Help() {
+		var name = typeof(Program).Assembly.GetName().Name;
+		Console.WriteLine($"Usage: {name} [options] path...");
+		Console.WriteLine("");
+		Console.WriteLine("-h  Show help");
+		Console.WriteLine("-V  Show version");
+		Console.WriteLine("-i  In-place edit");
+		Console.WriteLine("-r  Recur into directories");
+	}
+
 	static void Main(string[] args) {
-		Console.WriteLine("Hello, World!");
+	}
+
+	static void Version() {
+		var name = typeof(Program).Assembly.GetName().Name;
+		var version = typeof(Program).Assembly.GetName()?.Version?.ToString(2);
+		Console.WriteLine($"{name} {version}");
 	}
 }
