440f3793d6e3a3580bac6273d38b43ce5ce18594
Russell Wallace <russell.wallace@gmail.com>
2023-12-16 01:39:54 +0000

    main

--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
 class Program {
 	static void Help() {
 		var name = typeof(Program).Assembly.GetName().Name;
-		Console.WriteLine($"Usage: {name} [options] path...");
+		Console.WriteLine($"Usage: {name} [options]");
 		Console.WriteLine("");
 		Console.WriteLine("-h  Show help");
 		Console.WriteLine("-V  Show version");
-		Console.WriteLine("-i  In-place edit");
-		Console.WriteLine("-r  Recur into directories");
 	}
 
 	static void Main(string[] args) {
+		foreach (var arg in args) {
+			var s = arg;
+			if (s.StartsWith('-')) {
+				while (s.StartsWith('-'))
+					s = s[1..];
+				switch (s) {
+				case "?":
+				case "h":
+				case "help":
+					Help();
+					return;
+				case "V":
+				case "v":
+				case "version":
+					Version();
+					return;
+				default:
+					Console.Error.WriteLine("{0}: unknown option", arg);
+					Environment.Exit(1);
+					break;
+				}
+				continue;
+			}
+			Help();
+			return;
+		}
 	}
 
 	static void Version() {
