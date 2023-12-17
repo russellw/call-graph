07d6e79984216b17510c5935e0dd6aafa0a87ebc
Russell Wallace <russell.wallace@gmail.com>
2023-12-17 13:02:24 +0000

    struct

--- ClassLink.cs
+++ ClassLink.cs
@@ -3,9 +3,16 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 sealed class ClassLink: CSharpSyntaxWalker {
 	public override void VisitClassDeclaration(ClassDeclarationSyntax node) {
-		base.VisitClassDeclaration(node);
-
 		Html.Write("<li>");
 		Html.Link(node.Identifier.Text);
+
+		base.VisitClassDeclaration(node);
+	}
+
+	public override void VisitStructDeclaration(StructDeclarationSyntax node) {
+		Html.Write("<li>");
+		Html.Link(node.Identifier.Text);
+
+		base.VisitStructDeclaration(node);
 	}
 }
