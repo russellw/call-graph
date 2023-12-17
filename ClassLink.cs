d0b908c820b2ea979d903a80d6e702d9e3bea0b7
Russell Wallace <russell.wallace@gmail.com>
2023-12-17 13:27:11 +0000

    refactor

--- ClassLink.cs
+++ ClassLink.cs
@@ -1,18 +1,19 @@
-﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 sealed class ClassLink: CSharpSyntaxWalker {
 	public override void VisitClassDeclaration(ClassDeclarationSyntax node) {
-		Html.Write("<li>");
-		Html.Link(node.Identifier.Text);
-
+		Do(node);
 		base.VisitClassDeclaration(node);
 	}
 
 	public override void VisitStructDeclaration(StructDeclarationSyntax node) {
+		Do(node);
+		base.VisitStructDeclaration(node);
+	}
+
+	static void Do(TypeDeclarationSyntax node) {
 		Html.Write("<li>");
 		Html.Link(node.Identifier.Text);
-
-		base.VisitStructDeclaration(node);
 	}
 }
