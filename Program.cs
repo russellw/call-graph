5e78f95a327a637d103e11891ed4ba333919b616
Russell Wallace <russell.wallace@gmail.com>
2023-12-17 11:01:01 +0000

    exceptions in Main

--- Program.cs
+++ Program.cs
@@ -37,87 +37,96 @@ static class Program {
 	}
 
 	static void Main(string[] args) {
-		// Command line
-		foreach (var arg in args) {
-			var s = arg;
-			if (s.StartsWith('-')) {
-				while (s.StartsWith('-'))
-					s = s[1..];
-				switch (s) {
-				case "?":
-				case "h":
-				case "help":
-					Help();
-					return;
-				case "V":
-				case "v":
-				case "version":
-					Version();
-					return;
-				default:
-					Console.Error.WriteLine("{0}: unknown option", arg);
-					Environment.Exit(1);
-					break;
+		try {
+			// Command line
+			foreach (var arg in args) {
+				var s = arg;
+				if (s.StartsWith('-')) {
+					while (s.StartsWith('-'))
+						s = s[1..];
+					switch (s) {
+					case "?":
+					case "h":
+					case "help":
+						Help();
+						return;
+					case "V":
+					case "v":
+					case "version":
+						Version();
+						return;
+					default:
+						throw new Error(arg + ": unknown option");
+					}
 				}
-				continue;
+				Help();
+				return;
 			}
-			Help();
-			return;
+
+			// Project
+			var projectName = Path.GetFileNameWithoutExtension(ProjectFile());
+
+			// Build
+			Exec("dotnet", "clean /p:Configuration=Release /p:Platform=\"Any CPU\"");
+
+			// References
+			var compilation =
+				CSharpCompilation.Create(null).AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+			Descend(".", file => {
+				if (!Path.GetExtension(file).Equals(".dll", StringComparison.OrdinalIgnoreCase))
+					return;
+				compilation = compilation.AddReferences(MetadataReference.CreateFromFile(file));
+			});
+
+			// Source files
+			List<SyntaxTree> trees = new();
+			Descend(".", file => {
+				if (!Path.GetExtension(file).Equals(".cs", StringComparison.OrdinalIgnoreCase))
+					return;
+				var text = File.ReadAllText(file);
+				var tree = CSharpSyntaxTree.ParseText(text, CSharpParseOptions.Default, file);
+				if (tree.GetDiagnostics().Any()) {
+					foreach (var diagnostic in tree.GetDiagnostics())
+						Console.Error.WriteLine(diagnostic);
+					Environment.Exit(1);
+				}
+				trees.Add(tree);
+			});
+
+			// Header
+			Console.WriteLine("<!DOCTYPE html>");
+			Console.WriteLine("<html lang=\"en\">");
+			Console.WriteLine("<meta charset=\"utf-8\">");
+			Console.Write("<title>");
+			Console.Write(projectName);
+			Console.Write(" call graph");
+			Console.WriteLine("</title>");
+
+			// Contents
+			Console.WriteLine("<ul>");
+
+			Console.Write("<li>");
+			Etc.Link("classes");
+
+			Console.WriteLine("</ul>");
+
+			// Classes
+			Etc.Header(1, "classes");
+			foreach (var tree in trees) {
+				var model = compilation.AddSyntaxTrees(tree).GetSemanticModel(tree);
+				var root = tree.GetCompilationUnitRoot();
+			}
+		} catch (Error e) {
+			Console.WriteLine(e.Message);
+			Environment.Exit(1);
 		}
+	}
 
-		// Build
-		Exec("dotnet", "clean /p:Configuration=Release /p:Platform=\"Any CPU\"");
-
-		// References
-		var compilation =
-			CSharpCompilation.Create(null).AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
-		Descend(".", file => {
-			if (!Path.GetExtension(file).Equals(".dll", StringComparison.OrdinalIgnoreCase))
-				return;
-			compilation = compilation.AddReferences(MetadataReference.CreateFromFile(file));
-		});
-
-		// Source files
-		List<SyntaxTree> trees = new();
-		Descend(".", file => {
-			if (!Path.GetExtension(file).Equals(".cs", StringComparison.OrdinalIgnoreCase))
-				return;
-			var text = File.ReadAllText(file);
-			var tree = CSharpSyntaxTree.ParseText(text, CSharpParseOptions.Default, file);
-			if (tree.GetDiagnostics().Any()) {
-				foreach (var diagnostic in tree.GetDiagnostics())
-					Console.Error.WriteLine(diagnostic);
-				Environment.Exit(1);
-			}
-			trees.Add(tree);
-		});
-
-		// Header
-		Console.WriteLine("<!DOCTYPE html>");
-		Console.WriteLine("<html lang=\"en\">");
-		Console.WriteLine("<meta charset=\"utf-8\">");
+	static string ProjectFile() {
 		foreach (var entry in new DirectoryInfo(".").EnumerateFileSystemInfos())
-			if (Path.GetExtension(entry.Name).Equals(".csproj", StringComparison.OrdinalIgnoreCase)) {
-				Console.Write("<title>");
-				Console.Write(Path.GetFileNameWithoutExtension(entry.Name));
-				Console.WriteLine("</title>");
-				break;
-			}
-
-		// Contents
-		Console.WriteLine("<ul>");
-
-		Console.Write("<li>");
-		Etc.Link("classes");
-
-		Console.WriteLine("</ul>");
-
-		// Classes
-		Etc.Header(1, "classes");
-		foreach (var tree in trees) {
-			var model = compilation.AddSyntaxTrees(tree).GetSemanticModel(tree);
-			var root = tree.GetCompilationUnitRoot();
-		}
+			if (Path.GetExtension(entry.Name).Equals(".csproj", StringComparison.OrdinalIgnoreCase))
+				return entry.Name;
+		throw new Error(".csproj file not found");
 	}
 
 	static void Version() {
