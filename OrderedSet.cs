b3f3e1aecfaaf58b2cd510eb7bce4517c164fe56
Russell Wallace <russell.wallace@gmail.com>
2023-12-23 13:20:31 +0000

    fmt

--- OrderedSet.cs
+++ OrderedSet.cs
@@ -1,7 +1,7 @@
-﻿sealed class OrderedSet<T>: List<T> {
-	public new void Add(T item) {// In theory, this is O(N) per item
-								 // but in practice on modern hardware
-								 // it is fast for the anticipated range of N
-								 if (!Contains(item)) base.Add(item);
+﻿// In theory, this is O(N) per item
+// but in practice on modern hardware
+// it is fast for the anticipated range of N
+sealed class OrderedSet<T>: List<T> {
+	public new void Add(T item) { if (!Contains(item)) base.Add(item);
 }
 }
