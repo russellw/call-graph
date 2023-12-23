eff9297f42cfd6726f14ed25700fc0a02fc8b33b
Russell Wallace <russell.wallace@gmail.com>
2023-12-23 23:23:17 +0000

    type

--- Type.cs
+++ Type.cs
@@ -1,9 +1,40 @@
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 sealed class Type {
-	public Type(TypeDeclarationSyntax typeDeclaration) {
-		this.typeDeclaration = typeDeclaration;
+	public static void Add(TypeDeclarationSyntax typeDeclaration) {
+		var type = new Type(typeDeclaration);
+		types.Add(type.key, type);
 	}
 
-	TypeDeclarationSyntax typeDeclaration;
+	static readonly Dictionary<string, Type> types = new();
+
+	readonly string key;
+	readonly TypeDeclarationSyntax typeDeclaration;
+
+	Type(TypeDeclarationSyntax typeDeclaration) {
+		this.typeDeclaration = typeDeclaration;
+		key = DottedName();
+	}
+
+	string DottedName() {
+		SyntaxNode? a = typeDeclaration;
+		var parts = new List<string>();
+		for (;;) {
+			string name;
+			switch (a) {
+			case NamespaceDeclarationSyntax namespaceDeclaration:
+				name = namespaceDeclaration.Name.ToString();
+				break;
+			case TypeDeclarationSyntax typeDeclaration:
+				name = typeDeclaration.Identifier.ToString();
+				break;
+			default:
+				parts.Reverse();
+				return string.Join('.', parts);
+			}
+			a = a.Parent;
+			parts.Add(name);
+		}
+	}
 }
