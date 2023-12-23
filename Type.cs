b32c0ad7bf6c90c6af8720756dd0134146c52b5f
Russell Wallace <russell.wallace@gmail.com>
2023-12-23 15:41:51 +0000

    rename

---
+++ Type.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+sealed class Type
+    {
+    TypeDeclarationSyntax node;
+
+    public Type(TypeDeclarationSyntax node)
+    {
+        this.node = node;
+    }
+}
